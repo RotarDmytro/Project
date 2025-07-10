4060d8231729b10c8d3a42e90680f350c3aec990
RotarDmytro <rotardimaand@gmail.com>
2025-07-11 00:46:48 +0300

    The page layout and styling are complete, in addition to the adaptive layout, a slider has been created.

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    slidesToScroll: 1,
+    infinite: true,
+    dotsClass: 'slick-dots',
+    variableWidth: true,
+  });
+});
