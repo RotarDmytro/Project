82de2702d18af6ea186da82cec4eaf4cb8fb6838
RotarDmytro <rotardimaand@gmail.com>
2025-07-16 00:25:46 +0300

    Page layout completed, adaptive features made for DF, TF (HD), TF (SD). Slider, burger menu, scss used

--- app/js/all.js
+++ app/js/all.js
@@ -10,7 +10,26 @@ $(document).ready(function() {
     dots: true,
     slidesToScroll: 1,
     infinite: true,
-    dotsClass: 'slick-dots',
-    variableWidth: true,
+    dotsClass: 'slick-dots'
   });
+});
+
+var burger = document.querySelector('.header__hamburger a svg');
+var burger_active = document.querySelector('.header__hamburger');
+var menu = document.querySelector('.header__menu__bottom');
+var body = document.querySelector('body');
+var overlay = document.querySelector('.overlay');
+
+burger.addEventListener('click', function() {
+    menu.classList.toggle('show');
+    burger_active.classList.toggle('show');
+    body.classList.toggle('no-scroll');
+    overlay.classList.toggle('show');
+});
+
+overlay.addEventListener('click', function() {
+    menu.classList.toggle('show');
+    burger_active.classList.toggle('show');
+    body.classList.toggle('no-scroll');
+    overlay.classList.toggle('show');
 });
